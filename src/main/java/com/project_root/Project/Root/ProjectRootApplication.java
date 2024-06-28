bceec4895c87baee8b7ba61b0ec28dd5ff194441
DevJorgeSC <wxdevjorge@hotmail.com>
2024-06-28 12:05:28 -0300

    Primeiro commit.

---
+++ src/main/java/com/project_root/Project/Root/ProjectRootApplication.java
@@ -0,0 +1,13 @@
+package com.project_root.Project.Root;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project_root/Project/Root/ProjectRootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_root.Project.Root;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
